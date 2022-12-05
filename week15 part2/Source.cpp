d052cf1987f81bdd32bc66c738b283fe1f82b3ce
Aceramey <aceramey02@gmail.com>
2022-12-04 20:18:07 -0500

    it works pog

--- week15 part2/Source.cpp
+++ week15 part2/Source.cpp
@@ -2,16 +2,34 @@
 // CIS 1202
 // 12/4/2022
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 template <typename T>
 T half(T number)
 {
-	return number / 2;
+	if (typeid(number) == typeid(std::int32_t))
+	{
+		float temp;
+		temp = static_cast<double>(number);
+		temp = temp / 2.0;
+		temp = round(temp);
+		return static_cast<int>(temp);
+	}
+	else
+	{
+		return number / 2.0;
+	}
 }
 
 int main()
 {
-
+	double a = 7.0;
+	float b = 5.0f;
+	int c = 3;
+	
+	cout << half(a) << endl;
+	cout << half(b) << endl;
+	cout << half(c) << endl;
 }
